9fcf2ffa634621e15968ed265282620c3cbfe911
DamaFace <marketingdamaface@gmail.com>
2025-08-13 19:03:31 -0300

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -76,11 +76,12 @@ const Hero = () => {
             {/* Content */}
             <div className="lg:col-span-2">
               <h1 className="text-4xl md:text-5xl lg:text-6xl font-extrabold mb-6 leading-tight text-white drop-shadow-sm">
-                Preenchimento Labial
-                <span className="text-[#fa1571] block font-semibold">Aumente o volume <span className="text-[#fff] block font-semibold">e realce a beleza natural dos seus.</span> lábios.</span>
+                Rinomodelação
+                <span className="text-[#fa1571] block font-semibold">Corrija pequenas imperfeições <span className="text-[#fff] block font-semibold">e </span>harmonize seu perfil</span>
+                <span className="text-[#fa1571] block font-semibold">harmonize seu perfil</span> de forma natural
               </h1>
                 <p className="text-lg md:text-xl mb-8 text-gray-300 leading-relaxed max-w-2xl">
-                  São mais de <span className="text-[#fa1571] font-semibold">8 anos</span> transformando sorrisos com preenchimento labial seguro e natural. Realce o contorno, aumente o volume e conquiste lábios mais hidratados e harmoniosos, mantendo sua essência e beleza única.
+                  São mais de <span className="text-[#fa1571] font-semibold">8 anos</span> realizando rinomodelação segura e eficaz. Corrija desníveis, imperfeições ou assimetrias do nariz, valorizando a harmonia do rosto e mantendo sua naturalidade e expressão única.
                 </p>
               <div className="flex flex-wrap gap-3 mb-8">
               </div>
